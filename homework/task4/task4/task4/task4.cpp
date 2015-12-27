797e4e0ba47650c663e77d49a529bf636ca8364b
Vit <alfredfid@gmail.com>
2015-12-27 16:47:01 +0300

    task4 a1 a2 a3 b1

--- homework/task4/task4/task4/task4.cpp
+++ homework/task4/task4/task4/task4.cpp
@@ -20,6 +20,9 @@ public:
 	virtual void print() {
 		cout << "class A2: " << a2 << endl;
 	}
+	virtual void show() {
+		cout << "a2" << endl;
+	}
 protected:
 	int a2;
 };
@@ -30,9 +33,27 @@ public:
 	virtual void print() {
 		cout << "class A3: " << a3 << endl;
 	}
+	virtual void show() {
+		cout << "a3" << endl;
+	}
 protected:
 	int a3;
 };
+
+class B1 : public A1, A2, A3 {
+public:
+	B1(int bval, int a1val, int a2val, int a3val) : b1(bval), A1 (a1val), A2(a2val), A3(a3val) {};
+	virtual void print() {
+		cout << "class B1: " << b1 << endl;
+	}
+	virtual void show() {
+		cout << "b1, a1, a2, a3 " << endl;
+	}
+protected:
+	int b1;
+};
+
+
 
 int main() {
 	A1 aobj;
