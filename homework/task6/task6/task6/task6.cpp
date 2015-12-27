c7d97c4ca53d302f5815f5b58f84daf1b1fff0be
Vit <alfredfid@gmail.com>
2015-12-27 17:29:27 +0300

    task6

--- homework/task6/task6/task6/task6.cpp
+++ homework/task6/task6/task6/task6.cpp
@@ -5,6 +5,11 @@ using namespace std;
 #define WHAT_SIZE 2
 
 template <typename T>
+void print(T arg) {
+	std::cout << arg << std::endl;
+}
+
+template <typename T>
 int first_index_of(T* when, T* what, int index) {
 
 	for (int i = index; i < WHEN_SIZE; i++) {
@@ -31,9 +36,8 @@ int first_index_of(T* when, T* what, int index) {
 
 int main()
 {
-
 	char when[WHEN_SIZE] = { 'a', 'b', 'c', 'd', 'g', 'h' };
-	char what[WHAT_SIZE] = { 'd', 'g' };
+	char what[WHAT_SIZE] = { 'g', 'h' };
 	int ind = first_index_of(when, what, 2);
 
 	return 0;
