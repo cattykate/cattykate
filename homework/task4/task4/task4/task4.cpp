7baa8b639f93b30d20c43a626e38bd392a939d1a
Catty <Ekaterina.barkova95@yandex.ru>
2015-12-28 20:14:52 +0300

    with class C1

--- homework/task4/task4/task4/task4.cpp
+++ homework/task4/task4/task4/task4.cpp
@@ -42,7 +42,7 @@ protected:
 
 class B1 : public A1, A2, A3 {
 public:
-	B1(int bval, int a1val, int a2val, int a3val) : b1(bval), A1(a1val), A2(a2val), A3(a3val) {};
+	B1(int b1val, int a1val, int a2val, int a3val) : b1(b1val), A1(a1val), A2(a2val), A3(a3val) {};
 	virtual void print() {
 		cout << "class B1: " << b1 << endl;
 	}
@@ -55,7 +55,7 @@ protected:
 
 class B2 : public A1, A2, A3 {
 public:
-	B2(int bval, int a1val, int a2val, int a3val) : b2(bval), A1(a1val), A2(a2val), A3(a3val) {};
+	B2(int b2val, int a1val, int a2val, int a3val) : b2(b2val), A1(a1val), A2(a2val), A3(a3val) {};
 	virtual void print() {
 		cout << "class B2: " << b2 << endl;
 	}
@@ -68,9 +68,9 @@ protected:
 
 class B3 : public A1, A2, A3 { 
 public:
-	B3(int bval, int a1val, int a2val, int a3val) : b3(bval), A1(a1val), A2(a2val), A3(a3val) {};
+	B3(int b3val, int a1val, int a2val, int a3val) : b3(b3val), A1(a1val), A2(a2val), A3(a3val) {};
 	virtual void print() {
-		cout << "class B2: " << b3 << endl;
+		cout << "class B3: " << b3 << endl;
 	}
 	virtual void show(){
 		cout << "b3, a1, a2, a3 " << endl;
@@ -79,7 +79,21 @@ protected:
 	int b3;
 };
 
+class C1 : public B1, B2, B3 {
+public:
+	C1(int cval, int a1val, int a2val, int a3val, int b1val, int b2val, int b3val) : c1(cval), B1(b1val, a1val, a2val, a3val), B2(b2val, a1val, a2val, a3val), B3(b3val, a1val, a2val, a3val) {};
+	void print() {
+		cout << "class C1: " << c1 << endl;
+	}
+	void show(){
+		cout << "c1, b1, b2, b3" << endl;
+	}
+protected:
+	int c1;
+};
+
 
 int main() {
+
 
 }
