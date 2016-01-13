0ebafcffdda93b949f58df5b7b0b1ca20e30bae4
Catty <Ekaterina.barkova95@yandex.ru>
2016-01-13 17:43:05 +0300

    add function Bubble Sort

--- exam/Exam/several_tasks.c
+++ exam/Exam/several_tasks.c
@@ -1,7 +1,35 @@
 #include <stdlib.h>
+#define SIZE 7
 
-int main(){
+void sortBubble(int arr[], int size){
 
+	int temp = 0;
+
+	for (int i = 0; i < SIZE; i++){
+		for (int j = 0; j < SIZE; j++){
+			if (arr[j] < arr[j + 1]){
+				temp = arr[j + 1];
+				arr[j+1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < SIZE; i++){
+		printf("%d ", arr[i]);
+	}
+}
+
+int main()
+{
+	const int size = SIZE;
+	int arr[SIZE] = { 1, 2, 4, 5, 6, 7, 3 };
+
+	for (int i = 0; i < SIZE; i++){
+		printf("%d ", arr[i]);
+	}
+
+	printf("\nArray after sort \n");
+	sortBubble(arr, size);
 
 	return 0;
 }
