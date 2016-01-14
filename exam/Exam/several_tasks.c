476f45c47e0dfaeca2c1ee0af8a0441132e4b920
Catty <Ekaterina.barkova95@yandex.ru>
2016-01-14 20:20:28 +0300

    more tasks

--- exam/Exam/several_tasks.c
+++ exam/Exam/several_tasks.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#define SIZE 7
+#define SIZE 8
 
 void sortBubble(int arr[]){
 
@@ -75,27 +75,98 @@ void replaceSubstring(char str[], char substr[], char newsubstr[]){
 	}
 }
 
+void swapElem(int arr[]){
+	int temp = 0;
+	for (int i = 0; i < SIZE; i = i + 2)
+	{
+		temp = arr[i];
+		arr[i] = arr[i + 1];
+		arr[i + 1] = temp;
+	}
+	for (int i = 0; i < SIZE; i++){
+		printf("%d ", arr[i]);
+	}
+}
+
+void swapMinMax(int arr[]){
+	int max = arr[0];
+	int min = arr[0];
+	int posmin = 0;
+	int posmax = 0;
+	int temp = 0;
+
+	for (int i = 1; i < SIZE; i++){
+		if (arr[i] > max) {
+			max = arr[i];
+			posmax = i;
+		}
+	}
+	for (int i = 1; i < SIZE; i++){
+		if (arr[i] < min) {
+			min = arr[i];
+			posmin = i;
+		}
+	}
+		temp = arr[posmin];
+		arr[posmin] = arr[posmax];
+		arr[posmax] = temp;
+
+	for (int i = 0; i < SIZE; i++){
+		printf("%d ", arr[i]);
+	}
+}
+
+int calculateSum(int arr[]){
+	int sum = 0;
+	for (int i = 0; i < SIZE; i++){
+		sum = sum + arr[i];
+	}
+	return sum;
+}
+
+int calculateMax(int arr[]){
+	int max = arr[0];
+	for (int i = 1; i < SIZE; i++){
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
+int calculateSumPositiveOddInd(int arr[]){
+	int sum = 0;
+	for (int i = 1; i < SIZE; i = i + 2){
+		if (arr[i]>0) {
+			sum = sum + arr[i];
+		}
+	}
+	return sum;
+}
+
 int main()
 {
-	int arr[SIZE] = { 3, 2, 4, 5, 6, 7, 1 };
+	int arr[SIZE] = { 3, 2, 1, 5, 6, 7, 4, 8 };
 
 	for (int i = 0; i < SIZE; i++){
 		printf("%d ", arr[i]);
 	}
 
 	printf("\nResult \n");
-	sortBubble(arr);
+	//sortBubble(arr);
 	//sortSelection(arr);
 	//sortInsertion(arr);
-
+	//swapElem(arr);
+	swapMinMax(arr);
+	
 	char str[SIZE] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g' };
 	char substr[NSIZE] = { 'd', 'e' };
 	char newsubstr[NSIZE] = { 'o', 'k' };
 
-
-	printf("\nResult \n");
-	replaceSubstring(str, substr, newsubstr);
-
-
+	int res = 0;
+	//replaceSubstring(str, substr, newsubstr);
+	//res = calculateSum(arr);
+	//res = calculateSumPositiveOddInd(arr);
+	res = calculateMax(arr);
 	return 0;
 }
