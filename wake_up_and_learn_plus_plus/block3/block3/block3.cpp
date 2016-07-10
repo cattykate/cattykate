f6b810a77c5c77f44cac8099e5a70765e8439f9d
cattykate <ekaterina.barkova95@yandex.ru>
2016-07-10 15:41:57 +0300

    sq_root

--- wake_up_and_learn_plus_plus/block3/block3/block3.cpp
+++ wake_up_and_learn_plus_plus/block3/block3/block3.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main() {
-	int num = 0;
+	//int num = 0;
 
-	cout << "Enter the number from 1 to 3: ";
-	cin >> num;
-	
-	switch (num) {
-	case 1:
-		cout << "\nA bad beginning makes a bad ending\n";
-		break;
-	case 2:
-		cout << "A bird in the hand is worth two in the bush\n";
-		break;
-	case 3:
-		cout << "A cat in gloves catches no mice\n";
-		break;
-	default:
-		cout << "You can enter only 1, 2, 3\n";
+	//cout << "Enter the number from 1 to 3: ";
+	//cin >> num;
+	//
+	//switch (num) {
+	//case 1:
+	//	cout << "\nA bad beginning makes a bad ending\n";
+	//	break;
+	//case 2:
+	//	cout << "A bird in the hand is worth two in the bush\n";
+	//	break;
+	//case 3:
+	//	cout << "A cat in gloves catches no mice\n";
+	//	break;
+	//default:
+	//	cout << "You can enter only 1, 2, 3\n";
+	//}
+
+	double sq_root = 0;
+
+	for (int num = 1; num < 100; num++) {
+		sq_root = sqrt((double)num);
+		cout << num << " " << sq_root << endl;
 	}
+
+
 	system("pause");
 	return 0;
 	
