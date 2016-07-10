f2a416cb1820a078c585f24385b12723b002b972
cattykate <ekaterina.barkova95@yandex.ru>
2016-07-10 14:25:20 +0300

    simpleNums

--- wake_up_and_learn_plus_plus/simpleNums/simpleNums/simpleNums.cpp
+++ wake_up_and_learn_plus_plus/simpleNums/simpleNums/simpleNums.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 
 int main() {
-	 
-	bool isprime;
+	bool isprime = 0;
 
 	for (int i = 1; i <= 100; i++) {
+	
 		isprime = true;
+
 		for (int j = 2; j <= i / 2; j++) {
-			if (i%j == 0) isprime = false;
+			if ((i%j) == 0) isprime = false;
 		}
-		if (isprime)
-			cout << i << "prime.\n";
+			if (isprime) 
+			cout << i << " is prime \n";
 	}
+
 	system("pause");
 	return 0;
 }
