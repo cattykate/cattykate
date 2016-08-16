b0812c812a8c120938405e00a7e0d25f14b128ea
cattykate <ekaterina.barkova95@yandex.ru>
2016-08-16 18:16:17 +0300

    recursion

--- wake_up_and_learn_plus_plus/strRev/strRev/strRev.cpp
+++ wake_up_and_learn_plus_plus/strRev/strRev/strRev.cpp
@@ -1,31 +1,51 @@
+//#include <iostream>
+//
+//int main() {
+//
+//	char str[] = "This is the first str";
+//	char* start;
+//	char* end;
+//	
+//	int len = 0;
+//	char tmp = 0;
+//
+//	std::cout << "Source str: " << str << std::endl;
+//	len = strlen(str);
+//
+//	start = str;
+//	end = &str[len - 1];
+//
+//	while (start < end){
+//		tmp = *start;
+//		*start = *end;
+//		*end = tmp;
+//
+//		start++;
+//		end--;
+//	}
+//
+//	std::cout << "Reverse str: " << str << std::endl;
+//
+//	system("pause");
+//	return 0;
+//}
+
+
+/*recursion*/
 #include <iostream>
 
-int main() {
+int factr(int n) {
+	int answer = 0;
 
-	char str[] = "This is the first str";
-	char* start;
-	char* end;
-	
-	int len = 0;
-	char tmp = 0;
-
-	std::cout << "Source str: " << str << std::endl;
-	len = strlen(str);
-
-	start = str;
-	end = &str[len - 1];
-
-	while (start < end){
-		tmp = *start;
-		*start = *end;
-		*end = tmp;
-
-		start++;
-		end--;
+	if (n == 1) {
+		return 1;
 	}
+	answer = factr(n - 1)*n;
+	return answer;
+}
 
-	std::cout << "Reverse str: " << str << std::endl;
+int main(){
+	std::cout << "Factorial 4: " << factr(4);
 
-	system("pause");
 	return 0;
 }
