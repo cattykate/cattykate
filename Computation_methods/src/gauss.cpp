62a09da819b511b3e8bb4e74db5041743340fcf1
cattykate <ekaterina.barkova95@yandex.ru>
2017-02-17 19:42:11 +0300

    add matrix

---
+++ Computation_methods/src/gauss.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	const unsigned int line = 4;
+	const unsigned int column = 5;
+
+	double matrix[line][column] = {
+		{-2, -9, -3, 7, -26 },
+		{-7, 2, 2, 5, -25 },
+		{-6, 2, 0, 0, -16 },
+		{0, -3, 8, -3, 5}
+	};
+
+
+
+	return 0;
+}
