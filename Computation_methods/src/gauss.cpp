7b857b71862024763e11de97e6ff517caabd0426
cattykate <ekaterina.barkova95@yandex.ru>
2017-02-17 23:12:38 +0300

    add choosing general element and swapping lines

--- Computation_methods/src/gauss.cpp
+++ Computation_methods/src/gauss.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-	const unsigned int line = 4;
-	const unsigned int column = 5;
+	const unsigned int n = 4;
 
-	double matrix[line][column] = {
+
+	double matrix[n][n+1] = {
 		{-2, -9, -3, 7, -26 },
 		{-7, 2, 2, 5, -25 },
 		{-6, 2, 0, 0, -16 },
 		{0, -3, 8, -3, 5}
 	};
 
+	double max = 0;
+	int ind_max = 0;
+	double tmp = 0;
+	///// finding max elem
 
+	for (int i = 0; i < n; i++) {
+		for (int m = i; m < n; m++) {
+			if ((abs(matrix[m][i])) > max)
+			{
+				ind_max = m;
+				max = abs(matrix[m][i]);
+			}
+		}
+		max = 0.0;
+
+		if (ind_max != i)
+		{
+			for (int j = i; j < n + 1; j++)
+			{
+				tmp = matrix[ind_max][j];
+				matrix[ind_max][j] = matrix[i][j];
+				matrix[i][j] = tmp;
+			}
+
+
+		}
+	}
 
 	return 0;
 }
+
